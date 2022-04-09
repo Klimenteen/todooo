979311ed55caeba13c3747a87a680cfe16f79f66
klimenteen <kpetruchkin@gmail.com>
2022-04-09 23:33:30 +0700

    add new todo

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React from "react";
+import  classNames  from "classnames";
+import Badge from "../Badge/";
 
-import './ListStyle.scss'
+import "./ListStyle.scss";
 
-
-function List({items}) {
-
+function List({ items, isRemovable, onClick }) {
+  console.log(items, isRemovable);
   return (
     <div>
-       <ul className="list">
-         {items.map(item=>(
-         <li className={item.active? "active" : ""}>
-           <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}</i>
-           <span>{item.name} </span>
-         </li>
-         ) )}
-          
-        </ul>
+      <ul onClick={onClick} className="list">
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className={classNames(item.className, { active: item.active })}
+          >
+            <i>
+              {item.icon ? (
+                item.icon
+              ) : (
+               <Badge color={item.color}/>
+              )}
+            </i>
+            <span>{item.name} </span>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
